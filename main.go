9954ade77e266459236d5b69a5ae3ecf9447d8a1
SaurabPoudel <poudelsaurab20@gmail.com>
2023-10-24 19:21:08 +0545

    HTMX setup

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		tmpl, _ := template.New("").ParseFiles("template/index.html")
+		tmpl.ExecuteTemplate(w, "Base", nil)
+	})
+	http.ListenAndServe("localhost:3000", r)
+}
